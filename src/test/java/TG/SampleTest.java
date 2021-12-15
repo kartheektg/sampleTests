b4acaa66af6dbc5234fa7342970a21065def0352
Kartheek Kandi <kartheekkandi@Kartheeks-MacBook-Pro.local>
2021-12-15 16:32:49 +0000

    changed tests and added cache to yml

--- src/test/java/TG/SampleTest.java
+++ src/test/java/TG/SampleTest.java
@@ -1,4 +1,6 @@
 package TG;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -8,28 +10,66 @@ import static org.hamcrest.Matchers.*;
 
 public class SampleTest {
 
-        @Test
-        public void test_NumberOfCircuitsFor2017Season_ShouldBe20() {
-              given().
-                        when().
-                        get("http://ergast.com/api/f1/2017/circuits.json").
-                        then().
-                        assertThat().
-                        body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
-
-        }
+     final String baserURI = "https://reqres.in/api";
 
     @Test
-    public void test_APIWithBasicAuthentication_ShouldBeGivenAccess() {
+    public void test_ListUsersReturnsSixRecordsPerPage() {
+          given().
+                  baseUri(baserURI).
+          when().
+                  get("/users?page=2").
+          then().
+                  assertThat().
+                  statusCode(200).
+                  body("data.id",hasSize(6));
 
-        given().
-                auth().
-                preemptive().
-                basic("username", "password").
-                when().
-                get("http://path.to/basic/secured/api").
-                then().
-                assertThat().
-                statusCode(400);
     }
+
+    @Test
+    public void test_SingleUserNotFound() {
+        given().
+                baseUri(baserURI).
+        when().
+                get("/users/23").
+        then().
+                assertThat().
+                statusCode(404);
+    }
+
+    @Test
+    public void test_CreateAUser() {
+        String data = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+        Response response =
+      given().
+              baseUri(baserURI).
+              and().
+              body(data).
+        when().
+              post("/users").
+        then().
+              extract().response();
+        System.out.println(response.getBody().asString());
+        Assert.assertEquals(201,response.statusCode());
+    }
+
+    @Test
+    public void test_InvalidLogin() {
+        String data = "{ email: peter@klaven}" ;
+        Response response =
+                given().
+                        baseUri(baserURI).
+                        and().
+                        body(data).
+                        when().
+                        post("/login").
+                        then().
+                        extract().response();
+        Assert.assertEquals("Missing email or username",response.jsonPath().getString("error"));
+        Assert.assertEquals(400,response.statusCode());
+
+    }
+
 }
